dd2019ab31de246b277a805a9c88bd7d1014a99e
haw1k <ivan_veremeychuk@mail.ru>
2014-06-11 21:24:18 +0600

    Commit 11.06.2014 21 : 24

--- src/main/java/ru/astrea/logic/controller/main/consultation/ConsultationController.java
+++ src/main/java/ru/astrea/logic/controller/main/consultation/ConsultationController.java
@@ -1,12 +1,32 @@
-//package ru.astrea.logic.controller.main.consultation;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import ru.astrea.logic.service.ConsultationService;
-//
-//@Controller
-//public class ConsultationController {
-//    @Autowired
-//    ConsultationService consultationService;
-//
-//}
+package ru.astrea.logic.controller.main.consultation;
+
+import org.joda.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.astrea.logic.controller.form.JsonResponse;
+import ru.astrea.logic.entity.Consultation;
+import ru.astrea.logic.service.ConsultationService;
+
+@Controller
+public class ConsultationController {
+    @Autowired
+    ConsultationService consultationService;
+
+    @RequestMapping(value = "consultation/add",  method = RequestMethod.POST, consumes="application/json")
+    public @ResponseBody
+    String addConsultation(@RequestBody Consultation consultation) {
+        LocalDate dateTime = new LocalDate();
+        consultation.setCreationDate(dateTime);
+        consultation.setProcessed(false);
+
+
+        JsonResponse response = new JsonResponse();
+        consultationService.addConsultation(consultation);
+
+        return "ok";
+    }
+}
